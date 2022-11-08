b231111e6b49c098056c27e36abee4707bcc293d
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-11-08 14:05:41 -0600

    Add details to MovieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -30,6 +30,14 @@ export class MovieView extends React.Component {
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
         </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Name: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
       </div>
     );

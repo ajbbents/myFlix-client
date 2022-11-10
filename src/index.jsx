ff270d6ca5856ee85ac66b58b0db822c71facd25
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-11-10 15:45:37 -0600

    Import React Bootstrap to create page

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainView from './components/main-view/main-view';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 //import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
@@ -9,7 +12,9 @@ import './index.scss';
 class myFlixApplication extends React.Component {
   render() {
     return (
-      <MainView />
+      <Container>
+        <MainView />
+      </Container>
     );
   }
 }

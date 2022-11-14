7941c07813b3da5eaca88f0d60de093c16d61553
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-11-14 15:49:57 -0600

    Add react bootstrap

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -5,6 +5,9 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import { RegistrationView } from "../registration-view/registration-view";
+import { Card } from 'react-bootstrap';
+
+import './login-view.scss';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
@@ -19,37 +22,39 @@ export function LoginView(props) {
   };
 
   return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
+    <div className="login-view">
+      <Card className="text-center" style={{ width: '25rem', padding: '2rem' }}>
+        <Card.Body>
+          <Card.Title>Login</Card.Title>
+          <Card.Text>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-      <Button variant="secondary" type="button" onClick={RegistrationView}>
-        Not registered yet?
-      </Button>
-    </Form>
+            <Form>
+              <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+              </Form.Group>
+
+              <Button variant="primary" type="submit" onClick={handleSubmit}>
+                Submit
+              </Button>
+              <Button variant="secondary" type="button" onClick={RegistrationView}>
+                Not registered yet?
+              </Button>
+            </Form>
+
+          </Card.Text>
+        </Card.Body>
+
+      </Card>
+    </div>
   );
 }
 
 LoginView.propTypes = {
   onLoggedIn: PropTypes.func.isRequired,
 };
-
-{/* <label>
-Username:
-<input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-</label>
-<label>
-Password:
-<input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-</label>
-<button type="submit" onClick={handleSubmit}>Submit</button>
-<button type="button" onClick={RegistrationView}>Not registered yet?</button> */}

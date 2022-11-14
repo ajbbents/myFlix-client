56a2de95321fc013bee93028c25798d022bd3292
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-11-14 15:50:43 -0600

    Add react bootstrap

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import { Container, Row, Col, Form, Card, Navbar, Nav, Button } from 'react-bootstrap';
+
+import './registration-view.scss';
+
 export function RegistrationView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -16,25 +20,57 @@ export function RegistrationView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Birthday:
-        <input type="birthday" value={birthday} onChange={e => setBirthday(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Register</button>
-    </form>
+
+    <Container fluid className="registration-view">
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#home">BingeableFilms</Navbar.Brand>
+          <Nav className="navLinks">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#movies">Movies</Nav.Link>
+            <Nav.Link href="#users">Users</Nav.Link>
+            <Nav.Link href="#logout">Logout</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Card style={{ width: "62rem", padding: '5rem' }}>
+        <Card.Body>
+          <Card.Title>Register</Card.Title>
+          <Card.Text>
+
+            <Form>
+              <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control type="text" required placeholder="Create a username" onChange={e => setUsername(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control type="password" required placeholder="Password must be 8 characters" onChange={e => setPassword(e.target.value)} />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address:</Form.Label>
+                <Form.Control type="email" required placeholder="Enter Email" onChange={e => setEmail(e.target.value)} />
+                <Form.Text className="text.muted">We'll never share your email with anyone. Pinky promise.</Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBirthday">
+                <Form.Label>Birthday:</Form.Label>
+                <Form.Control type="Birthday" required placeholder="MM/DD/YYYY" onChange={e => setBirthday(e.target.value)} />
+              </Form.Group>
+
+              <Button variant="primary" type="submit" onClick={handleSubmit}>Register</Button>
+            </Form>
+
+
+          </Card.Text>
+        </Card.Body>
+
+      </Card>
+
+    </Container>
   );
 }
 

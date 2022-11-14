fac65d5276c39624a3a35d44e646e901c4cec29d
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-11-14 15:50:38 -0600

    Add react bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { Card } from "react-bootstrap";
 
 export class MovieView extends React.Component {
 
@@ -19,28 +20,35 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Name: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-      </div>
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+
+          <div className="movie-view">
+            <div className="movie-poster">
+              <img src={movie.ImagePath} />
+            </div>
+            <div className="movie-title">
+              <span className="label">Title: </span>
+              <span className="value">{movie.Title}</span>
+            </div>
+            <div className="movie-description">
+              <span className="label">Description: </span>
+              <span className="value">{movie.Description}</span>
+            </div>
+            <div className="movie-director">
+              <span className="label">Director: </span>
+              <span className="value">{movie.Director.Name}</span>
+            </div>
+            <div className="movie-genre">
+              <span className="label">Name: </span>
+              <span className="value">{movie.Genre.Name}</span>
+            </div>
+            <button onClick={() => { onBackClick(null); }}>Back</button>
+          </div>
+
+        </Card.Body>
+      </Card>
     );
   }
 }
@@ -59,7 +67,7 @@ MovieView.propTypes = {
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Description: PropTypes.string.isRequired
-    }).isRequired
+    }).isRequired,
   }).isRequired,
   onBackClick: PropTypes.func.isRequired
 };

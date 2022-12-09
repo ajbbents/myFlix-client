3437f5a80273888957799fdab8e2b6e3fe1136d3
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-12-09 09:48:38 -0600

    Create profile-view

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { Card, Button, Col, Row, Container } from "react-bootstrap";
+
+import { Link, Route } from "react-router-dom";
+import axios from "axios";
+
+export default class ProfileView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      Username: null,
+      Password: null,
+      Email: null,
+      Birthday: null,
+      FavoriteMovies: [],
+    };
+  }
+
+  componentDidMount() {
+    const accessToken = localStorage.getItem("token");
+    this.getUser(accessToken);
+  }
+
+  onRemoveFavorite = (movie) => {
+    const username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    console.log(movie)
+    axios
+      .delete(
+        `https://erin-real-squirrel.cyclic.app/users/${Username}/movies/${movie}`,
+        { headers: { Authorization: `Bearer ${token}}` } }
+      )
+      .then((response) => {
+        console.log(response);
+        alert("Movie was removed from favorites.");
+        this.componentDidMount();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  onLoggedOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    this.setState({
+      user: null,
+    });
+    window.open("/", "_self");
+  }
+
+  getUser = (token) => {
+    const Username = localStorage.getItem("user");
+    axios
+      .get(`https://erin-real-squirrel.cyclic.app/users/${Username}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        this.setState({
+          Username: response.data.Username,
+          Password: response.data.Password,
+          Email: response.data.Email,
+          Birthday: response.data.Birthday,
+          FavoriteMovies: response.data.FavoriteMovies,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  editUser = (e) => {
+    e.preventDefault();
+    const Username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    axios
+      .put(`https://erin-real-squirrel.cyclic.app/users/${Username}`,
+        {
+          Username: this.state.Username,
+          Password: this.state.Password,
+          Email: this.state.Email,
+          Birthday: this.state.Birthday,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          Username: response.data.Username,
+          Password: response.data.Password,
+          Email: response.data.Email,
+          Birthday: response.data.Birthday,
+        });
+
+        localStorage.setItem("user", this.state.Username);
+        const data = response.data;
+        console.log(data);
+        console.log(this.state.Username);
+        alert("Profile has been updated.");
+        window.open(`/users/&{Username}`, "_self");
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  onDeleteUser() {
+    const Username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+  }
+}

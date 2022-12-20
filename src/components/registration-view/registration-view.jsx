7e05749e3e9fd2d761aafd57c183a97ad2a46302
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-12-20 12:30:35 -0600

    adjust proptypes

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -123,5 +123,9 @@ export function RegistrationView(props) {
 }
 
 RegistrationView.propTypes = {
-  onRegistration: PropTypes.func.isRequired,
+  register: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired,
+  }),
 };

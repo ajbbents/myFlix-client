ccdec7beb87c0858f6370234277f95961a0c4b89
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-01-03 12:37:03 -0600

    Adjust movieCard to function correctly

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,25 +4,28 @@ import axios from "axios";
 import { Button, Card, Row } from 'react-bootstrap';
 
 import { Link } from "react-router-dom";
+import "./movie-card.scss";
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
+    const { movie, handleFavorite } = this.props;
     return (
       <Card>
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
-            <Button variant="link">Open</Button>
-          </Link>
-          <Link to={`/directors/${encodeURIComponent(movie.Director.Name)}`}>
+          <Card.Footer class="card-footer">
+            <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+              <Button variant="primary">Open</Button>
+            </Link>
+            {/* <Link to={`/directors/${encodeURIComponent(movie.Director.Name)}`}>
             <Button variant="link">Director</Button>
           </Link>
           <Link to={`/genres/${encodeURIComponent(movie.Genre.Name)}`}>
             <Button variant="link">Genre</Button>
-          </Link>
+          </Link> */}
+          </Card.Footer>
         </Card.Body>
       </Card>
     );

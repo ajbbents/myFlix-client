1f199def93d372c6d41fa85cdc0a089151a1f319
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-01-23 13:17:29 -0600

    implement redux and store

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import MainView from './components/main-view/main-view';
 import { MovieCard } from './components/movie-card/movie-card';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -7,13 +11,17 @@ import { Container, Row, Col } from 'react-bootstrap';
 //import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 //main component (will eventually use all the others)
 class myFlixApplication extends React.Component {
   render() {
     return (
-      <Container>
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
